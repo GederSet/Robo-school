321720bbb5aca6d9bec093a89759dec0920207a4
GederSet <grischastulov10@gmail.com>
2024-01-06 21:13:18 +0300

    create header

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 // Системные функции
 
 import './libs/menu_burger.js';
+import './libs/dynamic_adaptive.js';
 // import './libs/placeholder.js';
 // import './libs/spoiler.js';
 // import './libs/swiper.js';
 // import './libs/popup.js';
 // import './libs/scroll_animation.js';
-// import './libs/dynamic_adaptive.js';
 // import './libs/parallax.js';
 // import './libs/is_mobile.js';
 // import './libs/ripple.js';
